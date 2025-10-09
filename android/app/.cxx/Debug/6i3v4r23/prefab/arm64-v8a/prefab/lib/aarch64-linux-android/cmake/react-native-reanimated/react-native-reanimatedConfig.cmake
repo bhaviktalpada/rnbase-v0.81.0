acad0fb1e44fde59edd70bbb7ee8a1ff37cf9c9a
bhaviktalpada <bhaviktalpada@gmail.com>
2025-10-09 13:38:44 +0530

    library upgraded

--- android/app/.cxx/Debug/6i3v4r23/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++ android/app/.cxx/Debug/6i3v4r23/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -1,7 +1,6 @@
 if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+add_library(react-native-reanimated::reanimated INTERFACE IMPORTED)
 set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "/Users/Mackbook/Github/RN/rnbase-v0.81.0/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/1m4s116z/obj/arm64-v8a/libreanimated.so"
     INTERFACE_INCLUDE_DIRECTORIES "/Users/Mackbook/Github/RN/rnbase-v0.81.0/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
     INTERFACE_LINK_LIBRARIES ""
 )
--- android/app/.cxx/Debug/6i3v4r23/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-worklets/react-native-workletsConfig.cmake
+++ android/app/.cxx/Debug/6i3v4r23/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-worklets/react-native-workletsConfig.cmake
@@ -1,7 +1,6 @@
 if(NOT TARGET react-native-worklets::worklets)
-add_library(react-native-worklets::worklets SHARED IMPORTED)
+add_library(react-native-worklets::worklets INTERFACE IMPORTED)
 set_target_properties(react-native-worklets::worklets PROPERTIES
-    IMPORTED_LOCATION "/Users/Mackbook/Github/RN/rnbase-v0.81.0/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/501vm10m/obj/arm64-v8a/libworklets.so"
     INTERFACE_INCLUDE_DIRECTORIES "/Users/Mackbook/Github/RN/rnbase-v0.81.0/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
     INTERFACE_LINK_LIBRARIES ""
 )
